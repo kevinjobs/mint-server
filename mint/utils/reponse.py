5713ecdf1c0c78a150afde5e830fa198cfa45291
Kevin Jobs <me@kevinjobs.com>
2024-12-29 22:51:57 +0800

    🐛 修复部分错误

--- mint/utils/reponse.py
+++ mint/utils/reponse.py
@@ -1,20 +1,20 @@
-from mint.constants import RespCode
+from mint.constants import Response
 
 
 def save_success(data=None):
-    return response(RespCode.OK, "save success", data)
+    return response(Response.OK[0], "save success", data)
 
 
 def find_success(data):
-    return response(RespCode.OK, "find success", data)
+    return response(Response.OK[0], "find success", data)
 
 
 def del_success(data=None):
-    return response(RespCode.OK, "delete success", data)
+    return response(Response.OK[0], "delete success", data)
 
 
 def update_success(data=None):
-    return response(RespCode.OK, "update success", data)
+    return response(Response.OK[0], "update success", data)
 
 
 def response(code: int, msg: str, data=None):
